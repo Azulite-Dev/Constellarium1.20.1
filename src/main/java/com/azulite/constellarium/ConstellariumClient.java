60aaeaef564a5d974299b86336b54187c35a47df
Azulite <jrburrows@virginmedia.com>
2024-10-22 23:18:12 +0100

    New files

---
+++ src/main/java/com/azulite/constellarium/ConstellariumClient.java
@@ -0,0 +1,10 @@
+package com.azulite.constellarium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConstellariumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/azulite/constellarium/ConstellariumDataGenerator.java
@@ -0,0 +1,11 @@
+package com.azulite.constellarium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ConstellariumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
